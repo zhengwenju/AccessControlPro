45b5db894877d8d90bc429ea58e14575f3a0162d
zhengwenju <zhengwenju8@163.com>
2021-07-21 17:05:42 +0800

    dao

--- mjxt-web/src/main/java/com/sv/mjxt/web/tcp/service/TerminalService.java
+++ mjxt-web/src/main/java/com/sv/mjxt/web/tcp/service/TerminalService.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import com.sv.mjxt.web.dao.TerminalDao;
+import com.sv.mjxt.web.mapper.TerminalDao;
 import com.sv.mjxt.web.dao.model.Terminal;
 
 /**
